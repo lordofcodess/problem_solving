80c869a47e811ab28959948b6419cf2363802dc8
Semanyoh Nissi Kekeli <semanyohn@gmail.com>
2023-06-30 13:00:54 +0000

    Create vaild_mountain_array.py

---
+++ vaild_mountain_array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        n = len(arr)
+        if n < 3:
+            return False
+    
+        peak = 0
+        while peak < n - 1 and arr[peak] < arr[peak + 1]:
+            peak += 1
+    
+        if peak == 0 or peak == n - 1:
+            return False
+    
+        while peak < n - 1 and arr[peak] > arr[peak + 1]:
+            peak += 1
+    
+        return peak == n - 1
