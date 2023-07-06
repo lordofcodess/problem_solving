d71b93a831f6ec4bb348b311938188c22943a757
Semanyoh Nissi Kekeli <semanyohn@gmail.com>
2023-07-06 02:11:03 +0000

    Create counting_sort.py

---
+++ counting_sort.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def countingSort(arr):
+    freq = [0] * 100
+    
+    for x in arr:
+        freq[x] += 1
+    return freq
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
