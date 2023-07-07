276448c220fafca88a01f72cc7b2acc6d19547f2
Semanyoh Nissi Kekeli <semanyohn@gmail.com>
2023-07-07 11:59:22 +0000

    Update count_number_of_distinct_objects_reverse.py

--- count_number_of_distinct_objects_reverse.py
+++ count_number_of_distinct_objects_reverse.py
@@ -1,1 +1,15 @@
+class Solution:
+    def countDistinctIntegers(self, nums: List[int]) -> int:
+        d = []
+        for x in nums:
+            st = str(x)
+            reversed_num = int(st[::-1])
+            d.append(reversed_num)
 
+        for x in nums:
+            d.append(x)
+        unique_set = set()
+
+        for x in d:
+            unique_set.add(x)
+        return len(unique_set)
